78bb9e9505d01a92e8d4d137b735245aa07abe21
Muhammed Refaat <muhammedrefaat@gmail.com>
2023-08-01 14:27:06 +0300

    Task1# ITI   - Edit Text  + Button  + ViewBinding + OnClickListener .

--- app/src/main/java/com/example/iti_project/MainActivity.kt
+++ app/src/main/java/com/example/iti_project/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.iti_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.iti_project.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val emailText = binding.editTextEmail.text
+
+        binding.loginButton.setOnClickListener {
+            if (emailText.isNotEmpty()) {
+                Toast.makeText(this@MainActivity, R.string.login_success, Toast.LENGTH_LONG).show()
+                println(emailText.toString())
+            }else{
+                 Toast.makeText(this@MainActivity, R.string.login_failed, Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
+
 }
