e0797059266d23dbb484dfd1812cacb462deb80c
Muhammed Refaat <muhammedrefaat@gmail.com>
2023-08-03 15:32:50 +0300

    Task3# Checkbox and Radiobutton handling in a login form with its validation

--- app/src/main/java/com/example/iti_project/MainActivity.kt
+++ app/src/main/java/com/example/iti_project/MainActivity.kt
@@ -15,12 +15,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val emailText = binding.editTextLogin.text
+        val emailText = binding.inputUsername.text
+        val passwordText = binding.inputPassword.text
+        var gamingCheckbox = binding.gamingCk
+        var soccerCheckbox = binding.soccerCk
+        var pingPongCheckbox = binding.pingPongCk
+        val maleRadioBtn = binding.maleRadio
+        val femaleRadioBtn = binding.femaleRadio
+        var conditionToLogin = false
+        var gamingCheck = false
+        var gamingSoccerChecked = false
+
 
         binding.loginButton.setOnClickListener {
-            if (emailText.isNotEmpty()) {
-                Toast.makeText(this@MainActivity, R.string.login_success, Toast.LENGTH_LONG).show()
-                println(emailText.toString())
+            conditionToLogin = (emailText.isNotEmpty() &&  passwordText.isNotEmpty() &&
+                    (gamingCheckbox.isChecked || soccerCheckbox.isChecked || pingPongCheckbox.isChecked))
+                    && (maleRadioBtn.isChecked || femaleRadioBtn.isChecked)
+            gamingCheck = gamingCheckbox.isChecked && !soccerCheckbox.isChecked && !pingPongCheckbox.isChecked
+            gamingSoccerChecked = gamingCheckbox.isChecked && soccerCheckbox.isChecked && !pingPongCheckbox.isChecked
+
+            if (conditionToLogin) {
+                if(maleRadioBtn.isChecked){
+                    if(gamingCheck){
+                        Toast.makeText(this@MainActivity,"Welcome $emailText, Sports: ${gamingCheckbox.text} - Gender: ${maleRadioBtn.text}", Toast.LENGTH_LONG).show()
+                    }else if (gamingSoccerChecked){
+                        Toast.makeText(this@MainActivity,"Welcome $emailText , Sports: ${gamingCheckbox.text} -${soccerCheckbox.text} -  Gender: ${maleRadioBtn.text}", Toast.LENGTH_LONG).show()
+                    }
+
+                        Toast.makeText(this@MainActivity,"Welcome $emailText , Sports: ${gamingCheckbox.text} -${soccerCheckbox.text} - ${pingPongCheckbox.text} -  Gender: ${maleRadioBtn.text}", Toast.LENGTH_LONG).show()
+                }else{
+                    if(gamingCheck){
+                        Toast.makeText(this@MainActivity,"Welcome $emailText , Sports: ${gamingCheckbox.text} - Gender: ${femaleRadioBtn.text}", Toast.LENGTH_LONG).show()
+                    }else if (gamingSoccerChecked){
+                        Toast.makeText(this@MainActivity,"Welcome $emailText , Sports: ${gamingCheckbox.text} -${soccerCheckbox.text} - Gender:  ${femaleRadioBtn.text}", Toast.LENGTH_LONG).show()
+                    }
+
+                    Toast.makeText(this@MainActivity,"Welcome $emailText , Sports: ${gamingCheckbox.text} -${soccerCheckbox.text} - ${pingPongCheckbox.text} -  Gender: ${femaleRadioBtn.text}", Toast.LENGTH_LONG).show()
+                }
+
             }else{
                  Toast.makeText(this@MainActivity, R.string.login_failed, Toast.LENGTH_LONG).show()
             }
